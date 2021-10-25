61dcb78dad4401049e4cadab3cb64fb1fda5c30c
Michael Stahnke <stahnma@devops.af>
2021-10-25 00:46:23 -0500

    test(script): fix file not found test (#203)

--- handlers/script_test.go
+++ handlers/script_test.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"reflect"
+	"regexp"
 	"testing"
 
 	"github.com/target/flottbot/models"
@@ -37,8 +38,6 @@ func TestScriptExec(t *testing.T) {
 
 	varMissingScriptAction := newExecAction(`echo "${notest}"`)
 
-	cmdNotFound := newExecAction(`/bin/sh ./this/is/a/trap.sh`)
-
 	msgBeforeExit := newExecAction(`/bin/sh ../testdata/fail.sh`)
 
 	tests := []struct {
@@ -52,7 +51,6 @@ func TestScriptExec(t *testing.T) {
 		{"Error Script", args{args: errorScriptAction, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 1, Output: ""}, true},
 		{"Existing Var Script", args{args: varExistsScriptAction, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 0, Output: "echo"}, false},
 		{"Missing Var Script", args{args: varMissingScriptAction, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 1, Output: ""}, true},
-		{"Script does not exist", args{args: cmdNotFound, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 2, Output: "/bin/sh: 0: cannot open ./this/is/a/trap.sh: No such file"}, true},
 		{"StdOut before exit code 1", args{args: msgBeforeExit, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 1, Output: "error is coming"}, true},
 	}
 
@@ -65,6 +63,42 @@ func TestScriptExec(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ScriptExec() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestScriptExecWithRegex(t *testing.T) {
+	type args struct {
+		args models.Action
+		msg  *models.Message
+		bot  *models.Bot
+	}
+
+	bot := new(models.Bot)
+	simpleScriptMessage := models.NewMessage()
+
+	cmdNotFound := newExecAction(`/bin/sh ./this/is/a/trap.sh`)
+
+	tests := []struct {
+		name       string
+		args       args
+		want       *models.ScriptResponse
+		wantErr    bool
+		wantRegexp *regexp.Regexp
+	}{
+		{"Script does not exist", args{args: cmdNotFound, msg: &simpleScriptMessage, bot: bot}, &models.ScriptResponse{Status: 2}, true, regexp.MustCompile(`No such file`)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ScriptExec(tt.args.args, tt.args.msg, tt.args.bot)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ScriptExec() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantRegexp.MatchString(string(got.Output)) {
+				t.Errorf("ScriptExec() = %v, want %v", got.Output, "Regexp(`"+tt.wantRegexp.String()+"`)")
 			}
 		})
 	}
