d8841c44d6a23917ddab90f079e272a04b0f52f0
David May <1301201+wass3r@users.noreply.github.com>
2023-01-05 23:19:32 -0600

    chore(copyright): update copyright year (#408)

--- core/matcher.go
+++ core/matcher.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
 //
 // Use of this source code is governed by the LICENSE file in this repository.
 
