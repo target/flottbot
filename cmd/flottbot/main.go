49cfe91d06b3a17eb5462e2c62104900e4146d29
DaxJohnson <dax.johnson@target.com>
2025-11-11 11:38:30 -0600

    fix: linting

--- cmd/flottbot/main.go
+++ cmd/flottbot/main.go
@@ -39,7 +39,9 @@ func main() {
 	}
 
 	// set some early defaults for the logger
+	
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+	
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 	log.Logger = log.Output(os.Stdout).With().Logger()
